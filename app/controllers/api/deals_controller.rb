4ec2a0993daadcea2ee44c6ce115b622c79a6463
Luis Perichon <luisperichon@gmail.com>
2014-07-17 11:48:18 -0300

    add support for jsonp to api

--- app/controllers/api/deals_controller.rb
+++ app/controllers/api/deals_controller.rb
@@ -1,13 +1,18 @@
 class Api::DealsController < ApplicationController
   # Leave index.json PUBLIC
   [:authenticate_user!,:require_padma_account,:set_current_account,:set_timezone,:set_locale].each do |auth_filter|
-    skip_filter auth_filter, only: :index
+    skip_filter auth_filter, only: [:index, :show]
   end
 
   respond_to :json
 
   def index
     @deals = Deal.query(params[:q]).all
-    respond_with @deals
+    respond_with @deals, :callback => params[:callback]
+  end
+
+  def show
+  	@deal = Deal.find(params[:id])
+  	respond_with @deal, :callback => params[:callback]
   end
 end
